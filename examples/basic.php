932f3936d19c29a48209e74a130774c1fac1de61
Dariusz Półtorak <dariusz.poltorak@happyendians.pl>
2015-11-02 14:01:09 +0100

    colors example + color code fix

--- examples/basic.php
+++ examples/basic.php
@@ -4,5 +4,12 @@ require_once('./../vendor/autoload.php');
 
 use Dariuszp\CliProgressBar;
 
-$bar = new CliProgressBar(10);
-$bar->progress();
+$bar = new CliProgressBar(350);
+$bar->display();
+
+while($bar->getCurrentstep() < $bar->getSteps()) {
+    usleep(50000);
+    $bar->progress();
+}
+
+$bar->end();
--- src/Dariuszp/CliProgressBar.php
+++ src/Dariuszp/CliProgressBar.php
@@ -8,23 +8,33 @@ namespace Dariuszp;
  */
 class CliProgressBar
 {
-    protected $barLength = 100;
+    const COLOR_CODE_FORMAT = "\033[%dm";
+
+    /**
+     * @var int
+     */
+    protected $barLength = 40;
+
     /**
      * @var string|bool
      */
     protected $color = false;
+
     /**
      * @var int
      */
     protected $steps = 1;
+
     /**
      * @var int
      */
     protected $currentStep = 0;
+
     /**
      * @var string
      */
     protected $charEmpty = '░';
+
     /**
      * @var string
      */
@@ -33,14 +43,17 @@ class CliProgressBar
      * @var string
      */
     protected $defaultCharEmpty = '░';
+
     /**
      * @var string
      */
     protected $defaultCharFull = '▓';
+
     /**
      * @var string
      */
     protected $alternateCharEmpty = '_';
+
     /**
      * @var string
      */
@@ -48,8 +61,8 @@ class CliProgressBar
 
     public function __construct($steps = 100, $currentStep = 0, $forceDefaultProgressBar = false)
     {
-        $this->setProgressTo($currentStep);
         $this->setSteps($steps);
+        $this->setProgressTo($currentStep);
 
         // Windows terminal is unable to display utf characters and colors
         if (!$forceDefaultProgressBar && strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
@@ -68,29 +81,6 @@ class CliProgressBar
     }
 
     /**
-     * @return int
-     */
-    public function getSteps()
-    {
-        return $this->steps;
-    }
-
-    /**
-     * @param int $steps
-     * @return $this
-     */
-    public function setSteps($steps)
-    {
-        $steps = intval($steps);
-        if ($steps < 0) {
-            throw new \InvalidArgumentException('Steps amount must be 0 or above');
-        }
-
-        $this->steps = intval($steps);
-        return $this;
-    }
-
-    /**
      * @return $this
      */
     public function displayDefaultProgressBar()
@@ -98,6 +88,60 @@ class CliProgressBar
         $this->charEmpty = $this->defaultCharEmpty;
         $this->charFull = $this->defaultCharFull;
         return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setColorToDefault()
+    {
+        $this->color = false;
+        return $this;
+    }
+
+    /**
+     * @param $start
+     * @param $end
+     * @return $this
+     */
+    protected function setColor($start, $end) {
+        $this->color = array(
+            sprintf(self::COLOR_CODE_FORMAT, $start),
+            sprintf(self::COLOR_CODE_FORMAT, $end),
+        );
+        return $this;
+    }
+
+    public function setColorToBlack() {
+        return $this->setColor(30, 39);
+    }
+
+    public function setColorToRed() {
+        return $this->setColor(31, 39);
+    }
+
+    public function setColorToGreen() {
+        return $this->setColor(32, 39);
+    }
+
+    public function setColorToYellow() {
+        return $this->setColor(33, 39);
+    }
+
+    public function setColorToBlue() {
+        return $this->setColor(34, 39);
+    }
+
+    public function setColorToMagenta() {
+        return $this->setColor(35, 39);
+    }
+
+    public function setColorToCyan() {
+        return $this->setColor(36, 39);
+    }
+
+    public function setColorToWhite() {
+        return $this->setColor(37, 39);
     }
 
     /**
@@ -143,23 +187,7 @@ class CliProgressBar
     }
 
     /**
-     * @return string|bool
-     */
-    public function getColor()
-    {
-        return $this->color;
-    }
-
-    /**
-     * @param string|boolean $color
-     */
-    public function setColor($color)
-    {
-        $this->color = $color;
-    }
-
-    /**
-     * @param int $currentstep
+     * @param int $currentStep
      * @return $this
      */
     public function addCurrentStep($currentStep)
@@ -257,23 +285,6 @@ class CliProgressBar
         return $this;
     }
 
-    public function display()
-    {
-        print $this->draw();
-    }
-
-    /**
-     * @return string
-     */
-    public function draw()
-    {
-        $emptyValue = floor($this->getCurrentstep() / $this->getSteps()) * $this->getBarLength();
-        $fullValue = $this->getBarLength() - $emptyValue;
-        $prc = number_format(($this->getCurrentstep() / $this->getSteps()) * 100, 1, '.', ' ');
-
-        return sprintf("\r%$4s%$5s %$3f%% (%$1d/%$2d)", $this->getCurrentstep(), $this->getSteps(), $prc, $emptyValue, $fullValue);
-    }
-
     /**
      * @return int
      */
@@ -297,6 +308,56 @@ class CliProgressBar
         if ($this->currentStep > $this->getSteps()) {
             $this->currentStep = $this->getSteps();
         }
+        return $this;
+    }
+
+    public function display()
+    {
+        print $this->draw();
+    }
+
+    /**
+     * @return string
+     */
+    public function draw()
+    {
+        $fullValue = floor($this->getCurrentstep() / $this->getSteps() * $this->getBarLength());
+        $emptyValue = $this->getBarLength() - $fullValue;
+        $prc = number_format(($this->getCurrentstep() / $this->getSteps()) * 100, 1, '.', ' ');
+
+        $colorStart = '';
+        $colorEnd = '';
+        if ($this->color) {
+            $colorStart = $this->color[0];
+            $colorEnd = $this->color[1];
+        }
+
+        $bar = sprintf("%4\$s%5\$s %3\$.1f%% (%1\$d/%2\$d)", $this->getCurrentstep(), $this->getSteps(), $prc, str_repeat($this->charFull, $fullValue), str_repeat($this->charEmpty, $emptyValue));
+        return sprintf("\r%s%s%s", $colorStart, $bar, $colorEnd);
+    }
+
+    /**
+     * @return int
+     */
+    public function getSteps()
+    {
+        return $this->steps;
+    }
+
+    /**
+     * @param int $steps
+     * @return $this
+     */
+    public function setSteps($steps)
+    {
+        $steps = intval($steps);
+        if ($steps < 0) {
+            throw new \InvalidArgumentException('Steps amount must be 0 or above');
+        }
+
+        $this->steps = intval($steps);
+
+        $this->setCurrentStep($this->getCurrentstep());
 
         return $this;
     }
